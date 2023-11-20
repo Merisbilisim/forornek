506054869876beafb05e1bbe021e424f4395102c
Merisbilisim <145693807+Merisbilisim@users.noreply.github.com>
2023-11-20 10:40:44 +0300

    Add files via upload

---
+++ öğrenci arama/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace öğrenci_arama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan=txtAra.Text;
+            bool kontrol=false;
+            for(int i = 0; i <=18; i++)
+            {
+                if (aranan == listBox1.Items[i].ToString())
+                {
+                   kontrol=true;
+                    break;
+                    
+                }
+           
+            }
+            if (kontrol == true)
+            {
+                MessageBox.Show("ÖĞRENCİ BULUNDU");
+            }
+            else
+            {
+                MessageBox.Show("ÖĞRENCİ BULUNAMADI");
+            }
+            
+
+            
+        }
+    }
+}
